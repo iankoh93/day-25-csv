8bc93dd7f166f661a5714ecf91b56cca006a686f
iankoh93 <ian_koh93@hotmail.com>
2022-03-01 23:41:00 -0800

    Finished the name the states game

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import turtle
 import pandas
 
+
+def write_state(name_state, x, y):
+    timmy.goto(x=x, y=y)
+    timmy.write(f"{name_state}", move=False, font=("Arial", 8, "normal"), align="center")
+
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -8,14 +14,47 @@ screen.addshape(image)
 turtle.shape(image)
 screen.setup(width=760, height=520)
 
-guess = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+timmy = turtle.Turtle()
+timmy.penup()
+timmy.hideturtle()
+timmy.speed("fastest")
 
-# def get_mouse_click_coordinate(x, y):
-#     print(x, y)
-#
-#
-# turtle.onscreenclick(get_mouse_click_coordinate)
-# turtle.mainloop()
+game_is_running = True
+data = pandas.read_csv("50_states.csv")
+correct_guesses = []
+score = 0
 
+while game_is_running:
 
-# screen.exitonclick()
+    guess = screen.textinput(title=f"{score}/50 States guessed!", prompt="What's another state's name?")
+
+    for state in data.state:
+        if guess is None:
+            game_is_running = False
+        elif guess.title() in correct_guesses:
+            pass
+        elif guess.title() == state:
+            location = data[data.state == guess.title()]
+            write_state(name_state=state, x=int(location.x), y=int(location.y))
+            correct_guesses.append(state)
+            score += 1
+
+    if score >= 50:
+        game_is_running = False
+        print("Wow you got all of the states!")
+
+print(correct_guesses)
+print(f"Your final score is {score}")
+missed_states = []
+
+for state in data.state:
+    if state not in correct_guesses:
+        location = data[data.state == state]
+        missed_states.append(state)
+        write_state(name_state=state, x=int(location.x), y=int(location.y))
+
+missed_states_data = pandas.DataFrame(missed_states)
+missed_states_data.to_csv("states_to_learn.csv")
+
+screen.exitonclick()
+
